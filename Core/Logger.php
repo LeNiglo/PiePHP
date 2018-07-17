87a569f4b5ae433f0f0ced559f402e5088cc544e
Guillaume Lefrant <lefrantguillaume@gmail.com>
2018-07-17 17:49:20 +0200

    Added Logger

---
+++ Core/Logger.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Core;
+
+/**
+*
+*/
+class Logger
+{
+    const LOG_FILENAME = "../logs/piephp.log";
+    protected static $_instance = NULL;
+    private $handle = NULL;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$_instance)) {
+            self::$_instance = new Logger();
+        }
+        return self::$_instance;
+    }
+
+    public function error($str = '')
+    {
+        $this->write($str, 'ERROR');
+    }
+
+    public function warn($str = '')
+    {
+        $this->write($str, 'WARNING');
+    }
+
+    public function info($str = '')
+    {
+        $this->write($str, 'INFO');
+    }
+
+    public function debug($str = '')
+    {
+        $this->write($str, 'DEBUG');
+    }
+
+    private function write($str, $level)
+    {
+        $date = date("Y-m-d H:i:s");
+        fwrite($this->handle, "[{$date}][{$level}] {$str}" . PHP_EOL);
+    }
+
+    public function __construct() {
+        $this->handle = fopen(self::LOG_FILENAME, 'a+');
+    }
+
+    public function __destruct() {
+        fclose($this->handle);
+    }
+}
