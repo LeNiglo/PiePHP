3ad244de3dd1324f841863fd3b7196250a1b9f87
LeNiglo <lefrantguillaume@gmail.com>
2019-03-30 00:28:00 +0100

    Added navbar

--- src/View/layout.blade.php
+++ src/View/layout.blade.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>PiePHP</title>
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
-    </head>
-    <body>
-        <header>
-
-        </header>
-        <div class="container">
-            {!! $_view !!}
-        </div>
-        <footer></footer>
-        <script src="{{ asset('js/jquery.slim.min.js') }}" charset="utf-8"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" charset="utf-8"></script>
-    </body>
+<head>
+    <meta charset="utf-8" />
+    <title>PiePHP</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ route('/') }}">My Blog</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('/posts') }}">Posts</a>
+                    </li>
+                    @if ($_SESSION['user_id'])
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('/u') }}">My Profile</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('/login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('/register') }}">Register</a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div class="container pt-3">
+        {!! $_view !!}
+    </div>
+    <footer></footer>
+    <script src="{{ asset('js/jquery.slim.min.js') }}" charset="utf-8"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" charset="utf-8"></script>
+</body>
 </html>
