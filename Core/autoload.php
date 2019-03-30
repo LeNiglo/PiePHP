f065641f3d232a99704c77d53b650a7e38eae505
LeNiglo <lefrantguillaume@gmail.com>
2019-03-30 02:01:52 +0100

    php-cs-fix autoload

--- Core/autoload.php
+++ Core/autoload.php
@@ -3,19 +3,22 @@
 spl_autoload_register(function ($class) {
     $file = null;
     if (preg_match('/Core/', $class)) {
-        $file = implode(DIRECTORY_SEPARATOR,
+        $file = implode(
+            DIRECTORY_SEPARATOR,
         [
             '..', str_replace('\\', DIRECTORY_SEPARATOR, $class)
         ]
         ) . '.php';
     } elseif (preg_match('/(Controller|Model)$/', $class)) {
-        $file = implode(DIRECTORY_SEPARATOR,
+        $file = implode(
+            DIRECTORY_SEPARATOR,
         [
             '..', 'src', str_replace('\\', DIRECTORY_SEPARATOR, $class)
         ]
         ) . '.php';
     } else {
-        $file = implode(DIRECTORY_SEPARATOR,
+        $file = implode(
+            DIRECTORY_SEPARATOR,
         [
             '..', str_replace('\\', DIRECTORY_SEPARATOR, $class)
         ]
