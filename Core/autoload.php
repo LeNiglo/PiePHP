0be9c0bade92b56ab8b5bf7bdeae1843fc2b5898
Guillaume Lefrant <lefrantguillaume@gmail.com>
2020-02-21 23:38:33 +0100

    php-cs-fixer on autoload

--- Core/autoload.php
+++ Core/autoload.php
@@ -3,26 +3,11 @@
 spl_autoload_register(function ($class) {
     $file = null;
     if (preg_match('/Core/', $class)) {
-        $file = implode(
-            DIRECTORY_SEPARATOR,
-        [
-            str_replace('\\', DIRECTORY_SEPARATOR, $class),
-        ]
-        ).'.php';
+        $file = implode(DIRECTORY_SEPARATOR, [str_replace('\\', DIRECTORY_SEPARATOR, $class)]).'.php';
     } elseif (preg_match('/(Controller|Model)$/', $class)) {
-        $file = implode(
-            DIRECTORY_SEPARATOR,
-        [
-            'src', str_replace('\\', DIRECTORY_SEPARATOR, $class),
-        ]
-        ).'.php';
+        $file = implode(DIRECTORY_SEPARATOR, ['src', str_replace('\\', DIRECTORY_SEPARATOR, $class)]).'.php';
     } else {
-        $file = implode(
-            DIRECTORY_SEPARATOR,
-        [
-            str_replace('\\', DIRECTORY_SEPARATOR, $class),
-        ]
-        ).'.php';
+        $file = implode(DIRECTORY_SEPARATOR, [str_replace('\\', DIRECTORY_SEPARATOR, $class)]).'.php';
     }
     if (file_exists($file)) {
         include $file;
